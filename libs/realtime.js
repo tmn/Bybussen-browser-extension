8448945a344dcc8fa8e4da09b7b64d06a43360ea
Tri M. Nguyen <mail@trimn.net>
2011-06-26 03:03:18 +0200

    Iniate class methods. - get_stop_info() - get_stop_list()

--- libs/realtime.js
+++ libs/realtime.js
@@ -3,6 +3,14 @@
  * extends ByBuss with real-time data
  */
 
-function Realtime() {
+function Realtime(api_key) {
+	this.api_key = api_key;
+}
+
+Realtime.prototype.get_stop_info = function(holdeplass_id) {
+	
+}
+
+Realtime.prototype.get_stop_list = function() {
 	
 }
